de112962baf1d94d8a06208498b8cfc89b3c0b0c
Joseph W. Becher <drazisil@users.noreply.github.com>
2015-10-10 22:29:19 -0400

    Update and rename CpanelDDNSTest.php to Cpanel_DDNSTest.php
    Corrected class name

--- tests/Cpanel_DDNSTest.php
+++ tests/Cpanel_DDNSTest.php
@@ -8,7 +8,7 @@ class CpanelDDNSTest extends PHPUnit_Framework_TestCase
     public function testCanReadConfig()
     {
         // Arrange
-        $cp_ddns = new CpanelDDNS(1);
+        $cp_ddns = new Cpanel_DDNS(1);
 
         // Act
         $config = $a->fetchConfig();
